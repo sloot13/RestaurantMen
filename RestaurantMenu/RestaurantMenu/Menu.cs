7efa0ea965e3d92db1928d60765257e92bce0a95
Kevin Franklin <deerdance72@gmail.com>
2020-06-04 16:23:55 -0400

    final countdown

---
+++ RestaurantMenu/RestaurantMenu/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+        public List<MenuItem> Meals{ get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ RestaurantMenu/RestaurantMenu/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    public class MenuItem
+    {
+        public double Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool Recent { get; set; }
+    }
+}
